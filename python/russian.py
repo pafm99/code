fc8e456e7a7f3422eb80e79ff9ac8943e18320c6
Paul Franco <paulfranco@me.com>
2017-12-22 23:48:24 -0800

    Adding russian.py

---
+++ python/russian.py
@@ -0,0 +1,15 @@
+# faster than the naive algorithm
+
+def russian(a,b):
+  x = a 
+  y = b
+  z = 0
+  
+  while(x > 0):
+    if x % 2 == 1:
+      z = z + y
+      y = y << 1
+      x = x >> 1
+  return z
+  
+print(russian(1,2))
