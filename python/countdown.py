5a328c8f1c543c52c75469a7811c5f03e2d826b2
Paul Franco <paulfranco@me.com>
2017-12-23 00:01:31 -0800

    adding countdown.py

---
+++ python/countdown.py
@@ -0,0 +1,14 @@
+import math
+
+def time(n):
+  return 3 + 2 * math.ceil(n/5)
+
+def countdown(x):
+  y = 0
+  while x > 0:
+    x = x - 5
+    y = y + 1
+  print(y)
+
+print(countdown(50))
+print(time(50))
