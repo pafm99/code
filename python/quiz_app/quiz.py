c95a8c4b5aa77192f6b94b6fa5517d23dcffeb4d
Paul Franco <paulfranco@me.com>
2018-01-03 15:16:27 -0800

    starting quiz app

---
+++ python/quiz_app/quiz.py
@@ -0,0 +1,21 @@
+import datetime
+import random
+
+from questions import Add, multiply
+
+class Quiz:
+    questions = []
+    answers = []
+
+    def __init__(self):
+        # Generate 10 random questions with numbers from 1 to 10
+        # Add these questions into self.questions
+
+    def take_quiz(self):
+        # Log the Start Time
+        # Ask all of the questions
+        # Log if they got the question right
+        # Log the end time
+        # show a summary
+
+        
