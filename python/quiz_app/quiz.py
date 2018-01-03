bef24937f4c4eb18bedba26213467c5d97581b01
Paul Franco <paulfranco@me.com>
2018-01-03 15:29:38 -0800

    updated quiz app

--- python/quiz_app/quiz.py
+++ python/quiz_app/quiz.py
@@ -18,4 +18,29 @@ class Quiz:
         # Log the end time
         # show a summary
 
-        
+    def ask(self, question):
+        # log the start time
+        # capture the answer
+        # check the answer
+        # log the end time
+        # if the answer is right send back true
+        # otherwise, send back false
+        # send back the elapsed time
+
+    def total_correct(self):
+        # return the total # of correct answers
+        total = 0
+        for answer in self.answers:
+            if answer[0]:
+                total += 1
+        return total
+
+    def summary(self):
+        # print how many they got right and the total like 5/10
+        print("You got {} out of {} right".format(self.total_correct(), len(self.questions()))
+        # print the total time for the quiz like 30 seconds!
+        print("It took you {} second total".format((self.end_time-self.start_time).seconds))
+
+
+
+
