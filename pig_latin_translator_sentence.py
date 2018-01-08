81b6205ceb946ec955e9794a585f7e1769d01cb0
Paul Franco <paulfranco@me.com>
2018-01-08 00:39:28 -0800

    Create pig_latin_translator_sentence.py

---
+++ pig_latin_translator_sentence.py
@@ -0,0 +1,8 @@
+sentence = input("Enter a sentence: ")
+output = []
+for word in sentence.split():
+  if word[0] in 'aeiou':
+    output.append(word + 'way')
+  else:
+    output.append(word[1:] + word[0] + 'ay')
+print(' '.join(output))
