f479b17cc582a9442875f49d06cc3793c993743c
Paul Franco <paulfranco@me.com>
2018-01-22 21:56:14 -0800

    Update century_from_year.py

--- python/century_from_year.py
+++ python/century_from_year.py
@@ -1,4 +1,4 @@
 def centuryFromYear(year):
-    return (year) // 100 + 1    # 1 because 2017 is 21st century, and 1989 = 20th century
+    return 1 + (year - 1) // 100    # 1 because 2017 is 21st century, and 1989 = 20th century
 
-print(centuryFromYear(1905))  # --> 21
+print(centuryFromYear(1905))  
