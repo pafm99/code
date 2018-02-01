9eabe771c0ff3432c19cb5d436fff7daec11af67
Paul Franco <paulfranco@me.com>
2018-01-31 17:24:09 -0800

    Update flip_a_dict.py

--- python/flip_a_dict.py
+++ python/flip_a_dict.py
@@ -1,1 +1,9 @@
+# Flip a Dict
 
+x = {'a': 1, 'b': 2, 'c': 3}
+
+f = {value : key
+      for key, value in x.items()}
+print(f)
+
+# To flip a dict this way the keys must be unique, so if 2 keys have the same value then the dictionary will be shortened to the number of unique values. Also, the keys must be hashable.
