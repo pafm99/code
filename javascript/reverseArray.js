b2245eb5b64b744fa412f8b10a1a4bd292a03432
Paul Franco <paulfranco@me.com>
2018-02-15 01:28:18 -0800

    Update reverseArray.js

--- javascript/reverseArray.js
+++ javascript/reverseArray.js
@@ -9,3 +9,17 @@ function reverseArr(input) {
 
 var a = [3,5,7,8]
 var b = reverseArr(a);
+
+var x = [4,-10, 7, 9, 34, 52];
+
+function reverse (a) {
+  var b = []; 
+  counter = 0;
+  for (var i = a.length-1; i >= 0; i--) {
+    b[counter] = a[i];
+    counter += 1;
+  }
+  return b;
+}
+
+console.log(reverse(x));
