5c3f45faeb5436b9d584068299aeadfa56d5353c
Paul Franco <paulfranco@me.com>
2018-02-15 01:25:05 -0800

    Update reverse_array.py

--- python/reverse_array.py
+++ python/reverse_array.py
@@ -1,13 +1,3 @@
 var x = [4,-10, 7, 9, 34, 52];
 
-function reverse (a) {
-  var b = []; 
-  counter = 0;
-  for (var i = a.length-1; i >= 0; i--) {
-    b[counter] = a[i];
-    counter += 1;
-  }
-  return b;
-}
 
-console.log(reverse(x));
